587d52755f4db71c17b43655d02ec70251d70735
Islem Dj <islemas23@gmail.com>
2024-06-20 07:06:44 +0100

    New : Advanced Mode

---
+++ src/Routes/AdvancedMode/AdvancedMode.jsx
@@ -0,0 +1,12 @@
+
+const AdvancedMode = () => {
+  return (
+    <>
+      <div className="bg-Background h-screen flex flex-col justify-center items-center">
+        <h1 className="font-outfit font-bold text-gray-500 text-3xl">COMING SOON</h1>
+      </div>
+    </>
+  )
+}
+
+export default AdvancedMode
